d46f939ec01349afb3bb2ca3f3ad129e6102c72e
dvarsha15 <varshadudam12@gmail.com>
2022-08-23 19:04:53 +0530

    Added configurations and input data

---
+++ conf/payload.py
@@ -0,0 +1,19 @@
+#create payload for post
+
+def car_payload():
+    car_json= {
+                'name':'Punch',
+                'brand':'Tata',
+                'price_range':'12-18lacs',
+                'car_type':'suv'
+               }
+    return car_json
+
+def update_car_payload():
+    car_json= {
+                'name':'Punch',
+                'brand':'Tata',
+                'price_range':'10-15lacs',
+                'car_type':'suv'
+               }
+    return car_json
